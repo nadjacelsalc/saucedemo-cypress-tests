0e2ba9ef784c7eff80a69d8a74d16c7ab0a71dc8
nadjacelsalc <nadja.salc@outlook.com>
2025-12-26 20:18:10 +0100

    Added mochawesome reports for login tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,19 @@
-module.exports = {
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    html: true,
+    json: true,
+    overwrite: false,
+    charts: true,
+    embeddedScreenshots: true
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
   },
-};
+});
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,4 +14,8 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import 'cypress-mochawesome-reporter/register';
+
+
+//import './commands'
+//import 'cypress-mochawesome-reporter/register';
